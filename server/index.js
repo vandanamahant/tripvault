86ef53c38d05ea70ccd6e9997199c624c45a7fe2
vandanamahant <vandanavandana924@gmail.com>
2026-08-17 22:20:08 +0530

    Update CORS origins

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://tripvault-two.vercel.app/',
+  origin: 'https://tripvault-alpha.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
